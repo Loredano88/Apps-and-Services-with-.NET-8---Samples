dfb82472da498b171c7effd02c3128ed43a46974
Loredano <loredano.rapari@tecnoin.eu>
2024-09-24 18:29:58 +0200

    Chapter16

---
+++ Chapter08/Northwind.WebApi.Service/WebApplication.Extensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.EntityModels;
+
+namespace Packt.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+    }
+}
